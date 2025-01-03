faf527b8fffebe732c2d6f24e97872f7ee6aaefc
Serezha1993 <007serzh2014@mail.ru>
2025-01-03 19:28:34 +0300

    ауфегкув ыдшвук

--- js/app.js
+++ js/app.js
@@ -13,3 +13,24 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+
+// Селекторы слайдов
+const slides = document.querySelectorAll('.slider-item');
+let currentIndex = 0;
+
+// Функция смены слайда
+function changeSlide() {
+    // Убираем класс active с текущего слайда
+    slides[currentIndex].classList.remove('active');
+
+    // Переходим к следующему слайду (циклически)
+    currentIndex = (currentIndex + 1) % slides.length;
+
+    // Добавляем класс active следующему слайду
+    slides[currentIndex].classList.add('active');
+}
+
+// Запускаем смену слайдов каждые 5 секунд
+setInterval(changeSlide, 5000);
